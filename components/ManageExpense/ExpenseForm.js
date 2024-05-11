823f2921be8343967ce2173025de18e3ac9dd4d7
Evan <evanz1215@gmail.com>
2024-05-11 20:19:14 +0800

    feat: Update ExpenseForm component to handle user input
    This commit updates the ExpenseForm component to handle user input for the amount, date, and description fields. It adds state management using the useState hook to track the input values and a corresponding inputChangeHandler function to update the values when the user types. This improves the functionality of the ExpenseForm and allows users to enter their expense details.

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,36 +1,54 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import Input from "./Input";
 
 const ExpenseForm = () => {
-  const amountChangeHandler = (text) => {
-    console.log(text);
+  const [inputValues, setInputValues] = useState({
+    amount: "",
+    date: "",
+    description: "",
+  });
+
+  const inputChangeHandler = (inputIdentifier, enteredValue) => {
+    setInputValues((curInputValues) => {
+      return {
+        ...curInputValues,
+        [inputIdentifier]: enteredValue,
+      };
+    });
   };
 
-  const dateChangeHandler = (text) => {};
-
   return (
-    <View>
-      <Input
-        label="Amount"
-        textInputConfig={{
-          keyboardType: "decimal-pad",
-          onChangeText: amountChangeHandler,
-        }}
-      />
-      <Input
-        label="Date"
-        textInputConfig={{
-          placeholder: "YYYY-MM-DD",
-          maxLength: 10,
-          onChangeText: () => {},
-        }}
-      />
+    <View style={styles.form}>
+      <Text style={styles.title}>Your Expense</Text>
+      <View style={styles.inputsRow}>
+        <Input
+          label="Amount"
+          style={styles.rowInput}
+          textInputConfig={{
+            keyboardType: "decimal-pad",
+            onChangeText: inputChangeHandler.bind(this, "amount"),
+            value: inputValues.amount,
+          }}
+        />
+        <Input
+          label="Date"
+          style={styles.rowInput}
+          textInputConfig={{
+            placeholder: "YYYY-MM-DD",
+            maxLength: 10,
+            onChangeText: inputChangeHandler.bind(this, "date"),
+            value: inputValues.date,
+          }}
+        />
+      </View>
       <Input
         label="Description"
         textInputConfig={{
           multiline: true,
           // autoCorrect: false,
+          onChangeText: inputChangeHandler.bind(this, "description"),
+          value: inputValues.description,
         }}
       />
     </View>
@@ -39,4 +57,22 @@ const ExpenseForm = () => {
 
 export default ExpenseForm;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: {
+    marginTop: 40,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#fff",
+    textAlign: "center",
+    marginVertical: 24,
+  },
+  inputsRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rowInput: {
+    flex: 1,
+  },
+});
--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -1,15 +1,43 @@
 import { StyleSheet, Text, TextInput, View } from "react-native";
 import React from "react";
+import { GlobalStyles } from "../../constants/styles";
 
-const Input = ({ label, textInputConfig }) => {
+const Input = ({ label, style, textInputConfig }) => {
+  const inputStyle = [styles.input];
+
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyle.push(styles.inputMultiline);
+  }
+
   return (
-    <View>
-      <Text>{label}</Text>
-      <TextInput {...textInputConfig} />
+    <View style={[styles.inputContainer, style]}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput {...textInputConfig} style={inputStyle} />
     </View>
   );
 };
 
 export default Input;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 16,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    color: GlobalStyles.colors.primary700,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+  },
+  inputMultiline: {
+    height: 100,
+    textAlignVertical: "top",
+  },
+});
